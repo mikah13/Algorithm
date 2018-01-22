0b4150b72b915181345ff16b1a436b5c278a80e7
mikah <anhminhhoang13@gmail.com>
2018-01-21 23:05:13 -0800

    Added MergeSort

---
+++ Sorting/MergeSort.js
@@ -0,0 +1,23 @@
+pushAndShift = (a, b) => a.push(b.shift());
+
+merge = (a, b) => {
+  let k = [];
+  while (a.length && b.length) {
+    pushAndShift(k, a[0] > b[0] ? b : a);
+  }
+  while (a.length) {
+    pushAndShift(k, a);
+  }
+  while (b.length) {
+    pushAndShift(k, b);
+  }
+  return k;
+};
+
+mergeSort = a => {
+  let middle = parseInt(a.length / 2);
+  if (a.length === 1) return a;
+  let x = mergeSort(a.slice(0, middle)); // left
+  let y = mergeSort(a.slice(middle, a.length)); // right
+  return merge(x, y);
+};
